6a03ef6f78679c21098ffa4d03a0a42dbf82e08e
mivanit <mivanits@umich.edu>
2022-03-24 13:03:00 -0600

    oops didnt add the extension code

---
+++ extension/build/extension.py
@@ -0,0 +1,42 @@
+# Built using vscode-ext
+
+import sys
+
+import vscode
+
+from dendron_citations.refs_vault_gen import gen
+
+vscfg_cfg_path = vscode.Config(
+	name = 'dendron_citations_cfg_path', 
+	description = 'configuration file path for dendron_citations', 
+	input_type = str, 
+	default = "dc_config.json",
+)
+
+ext : vscode.Extension = vscode.Extension(
+	name = "dendron_citations",
+	display_name = "dendron_citations",
+	version = 'v0.0.1',
+	config = [vscfg_cfg_path],
+)
+
+@ext.event
+async def on_activate():
+	vscode.log(f"The Extension '{ext.name}' has started")
+	vscode.InfoMessage(f"'{ext.name}' has started")
+
+
+@ext.command(name = "Generate Dendron citation notes")
+async def gen_notes(ctx):
+	gen(cfg_path = vscfg_cfg_path.value)
+	return await ctx.show(vscode.InfoMessage(f"{ext.name} is generating citation notes..."))
+
+
+
+
+
+
+def ipc_main():
+    globals()[sys.argv[1]]()
+
+ipc_main()
